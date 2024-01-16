8b7fd3b04dfb897d6040ebefcf8aaa9b1aa5156a
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2024-01-16 17:09:39 +0530

    add mint and trade token

--- contracts/contracts/polygon/GHOFanTokenMint.sol
+++ contracts/contracts/polygon/GHOFanTokenMint.sol
@@ -2,6 +2,79 @@
 pragma solidity ^0.8.10;
 
 
-contract GhoFanTokenMint{
-    
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GhoFanTokenMint is ERC1155URIStorage, Ownable {
+
+    struct Token {
+        string name;
+        string symbol;
+        string uri;
+        address creator;
+        uint256 mintPrice;
+    }
+
+    uint256 private tokenIdCounter;
+    address private immutable i_ghoFundMeModule;
+
+    string constant public  NAME = "GhoFanTokenMint";
+    string constant public  VERSION = "v0.0.1";
+
+    mapping(uint256 => Token) private _tokens;
+
+    constructor(address ghoFundMeModule) ERC1155("") {
+        i_ghoFundMeModule = ghoFundMeModule;
+        _transferOwnership(ghoFundMeModule);
+    }
+
+
+    function createToken(string memory name,string memory symbol,string memory uri, address creator, uint256 mintPrice) external onlyOwner returns (uint256) {
+        uint256 tokenId=totalSupply();
+        _tokens[tokenId] = Token(name, symbol, uri, creator, mintPrice);
+        _setURI(tokenId, uri);
+        return tokenId;
+    }
+
+    function mintToken(uint256 tokenId,uint256 amount,address recipient) external onlyOwner {
+        _mint(recipient, tokenId, amount, "");
+    }
+
+    function burnToken(uint256 tokenId,uint256 amount,address recipient) external onlyOwner {
+        _burn(recipient, tokenId, amount);
+    }
+
+    function _safeTransferFrom(
+        address ,
+        address ,
+        uint256 ,
+        uint256 ,
+        bytes memory 
+    ) internal virtual override {
+        revert("Soulbound Token: Disabled");
+    }
+
+    function _safeBatchTransferFrom(
+        address ,
+        address ,
+        uint256[] memory ,
+        uint256[] memory ,
+        bytes memory 
+    ) internal virtual override {
+        revert("Soulbound Token: Disabled");
+    }
+
+
+    function getToken(uint256 tokenId) external view returns (Token memory) {
+        return _tokens[tokenId];
+    }
+
+    function getModuleAddress() external view returns (address) {
+        return i_ghoFundMeModule;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return tokenIdCounter;
+    }
 }
--- contracts/contracts/polygon/GHOFanTokenTrade.sol
+++ contracts/contracts/polygon/GHOFanTokenTrade.sol
@@ -1,6 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract GhoFanTokenTrade{
-    
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GhoFanTokenTrade is ERC1155URIStorage, Ownable {
+
+    struct Token {
+        string name;
+        string symbol;
+        string uri;
+        address creator;
+        uint256 mintPrice;
+    }
+
+    uint256 private tokenIdCounter;
+    address private immutable i_ghoFundMeModule;
+
+    string constant public  NAME = "GhoFanTokenTrade";
+    string constant public  VERSION = "v0.0.1";
+
+    mapping(uint256 => Token) private _tokens;
+
+    constructor(address ghoFundMeModule) ERC1155("") {
+        i_ghoFundMeModule = ghoFundMeModule;
+        _transferOwnership(ghoFundMeModule);
+    }
+
+    function createToken(string memory name,string memory symbol,string memory uri, address creator, uint256 mintPrice) external onlyOwner returns (uint256) {
+        uint256 tokenId=totalSupply();
+        _tokens[tokenId] = Token(name, symbol, uri, creator, mintPrice);
+        _setURI(tokenId, uri);
+        return tokenId;
+    }
+
+    function mintToken(uint256 tokenId,uint256 amount,address recipient) external onlyOwner {
+        _mint(recipient, tokenId, amount, "");
+    }
+
+    function burnToken(uint256 tokenId,uint256 amount,address recipient) external onlyOwner {
+        _burn(recipient, tokenId, amount);
+    }
+
+    function getToken(uint256 tokenId) external view returns (Token memory) {
+        return _tokens[tokenId];
+    }
+
+    function getModuleAddress() external view returns (address) {
+        return i_ghoFundMeModule;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return tokenIdCounter;
+    }
 }
