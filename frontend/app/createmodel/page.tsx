9ccc5fef8b2ad9ad6a2650afa6876fc2cd008178
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-01-17 13:58:33 +0530

    createModelPage

--- frontend/app/createmodel/page.tsx
+++ frontend/app/createmodel/page.tsx
@@ -1,6 +1,128 @@
-import { NextPage } from "next"
+import type { NextPage } from "next";
 
 const page: NextPage = () => {
-    return(<>Hello</>)
-}
-export default page
+  return (
+    <div className="relative bg-gray-200 w-full h-screen overflow-hidden text-left text-5xl text-white font-monument-extended">
+      {/* <div className="absolute top-[-241px] left-[1366px] w-[1366px] h-[1009px] [transform:_rotate(180deg)] [transform-origin:0_0]" /> */}
+      <img
+        className="absolute top-[0px] left-[0px] w-screen h-screen object-cover"
+        alt=""
+        src="/2303w018n0021741ap30-1@2x.png"
+      />
+      <div className="absolute top-[0px] left-[930px] bg-[#131212] w-[1100px] h-screen" />
+      <img
+        className="absolute top-[28px] left-[51px] w-[422px] h-[123px] overflow-hidden"
+        alt=""
+        src="/ghfundme-1.svg"
+      />
+      <div className="absolute top-[42px] left-[1810px] w-14 h-14">
+        <div className="absolute top-[0px] left-[0px]w-14 h-14" />
+        <div className="absolute top-[21px] left-[14px] w-[29px] h-3.5">
+          <div className="absolute top-[-1.5px] left-[-1.5px] box-border w-8 h-[3px] border-t-[3px] border-solid border-white" />
+          <div className="absolute top-[5.5px] left-[-1.5px] box-border w-8 h-[3px] border-t-[3px] border-solid border-white" />
+          <div className="absolute top-[12.5px] left-[-1.5px] box-border w-8 h-[3px] border-t-[3px] border-solid border-white" />
+        </div>
+      </div>
+      <div className="absolute top-[230px] left-[110px] w-[763px] h-[612px] font-sans">
+        <div className="absolute top-[0px] left-[0px] rounded-lg bg-[#599E40] opacity-25 w-[763px] h-[612px]" />
+        <img
+          className="absolute top-[37px] left-[85px] w-[151px] h-[159.9px] overflow-hidden"
+          alt=""
+          src="/metamask-1.svg"
+        />
+        <img
+          className="absolute h-[29.51%] w-[32.22%] top-[13.59%] right-[13.61%] bottom-[56.89%] left-[54.17%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="/vectorlens.svg"
+        />
+        <div className="absolute top-[258px] left-[85px] inline-block w-[399px] h-[72px]">
+          <p className="m-0">Lens ID : 1</p>
+          <p className="m-0">Username: Lens</p>
+        </div>
+        <div className="absolute top-[213px] left-[159px] text-xl inline-block w-[246px] h-6">
+          0x89c........7d21f111
+        </div>
+        <div className="absolute top-[337px] left-[361px] w-[167px] h-[41.3px]">
+          <div className="absolute top-[0px] left-[0px] bg-forestgreen-200 w-[167px] h-[41.3px]" />
+          <img
+            className="absolute top-[15.9px] left-[138px] rounded-6xs w-6 h-[24.4px]"
+            alt=""
+            src="/arrowbigcornerlinedown.svg"
+          />
+          <div className="absolute top-[5.3px] left-[9px] inline-block w-[129px] h-[23.3px]">
+            LOGOUT
+          </div>
+        </div>
+      </div>
+      
+      <div className="absolute top-[151px] left-[1116px] w-[615px] h-[603px] text-xl text-gray-100 font-sans">
+      
+        <div className="absolute top-[0px] left-[0px] rounded-xl  box-border w-[594px] h-[603px] border-[2px] border-dashed border-[#025231]" />
+        <div className="absolute top-[49px] left-[56px] text-[28px] text-white inline-block w-[379px] h-[68px]">
+          Create Subscription Model
+        </div>
+        
+        
+        <form>
+
+        <input
+                type="number"
+                name="price"
+                placeholder="Base Price"
+                className="absolute leading-[1.6] top-[404px] left-[66px] w-[353px] h-8 px-4 py-2 border-b-2 border-white outline-none  focus:white bg-transparent"
+            />
+            <div>
+            <p className="absolute leading-[1.6] top-[404px] left-[430px] text-lg">
+            GHO
+            </p>    
+            </div> 
+        <input
+                type="text"
+                name="code"
+                placeholder="Token Code"
+                className="absolute top-[221px] left-[223px] w-[317px] h-8 px-4 py-2 border-b-2 border-white outline-none  focus:white bg-transparent"
+            /> 
+            <input
+                type="text"
+                name="desc"
+                placeholder="Token Description"
+                className="absolute top-[320px] left-[66px] w-[474px] h-8 px-4 py-2 border-b-2 border-white outline-none  focus:white bg-transparent"
+            />        
+            <input
+                    type="text"
+                    name="name"
+                    placeholder="Token Name"
+                    className="absolute top-[159px] left-[223px] w-[317px] h-8 px-4 py-2 border-b-2 border-white outline-none  focus:white bg-transparent"
+                />
+                <select placeholder="Duration" className="absolute top-[489px] left-[66px] w-[352px] h-[46px] appearance-none bg-transparent border border-gray-200 text-[#9CA3AF] py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-transparent focus:border-gray-500" id="grid-state">
+                <option>Duration</option>
+                <option>2 Months</option>
+                <option>6 Months</option>
+                </select>
+        </form>
+        
+        <div className="absolute top-[141px] left-[24px] w-[199px] h-[193px] text-center text-[15px]">
+          <div className="absolute top-[0px] left-[32px] rounded-[31px] bg-forestgreen-100 w-[132px] h-[132px]" />
+          <img
+            className="absolute top-[10px] left-[35px] w-[140px] h-[140px] cursor-pointer py-6 rounded-lg 
+            transform transition duration-500 
+            hover:scale-105"
+            alt=""
+            src="/plus.svg"
+          />
+        </div>
+        <div className="absolute top-[497px] left-[483px] w-[76px] h-[72px] transform transition duration-500 
+            hover:scale-105">
+          <div className="absolute top-[0px] left-[0px] bg-forestgreen-200 w-[76px] h-[72px]" />
+          <img
+            className="absolute top-[15px] left-[17px] rounded-[1px] w-[42px] h-[42px]"
+            alt=""
+            src="/vector-27.svg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
