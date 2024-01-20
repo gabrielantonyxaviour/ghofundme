ce2128373774b8cd19a492039f4d963e12d333b0
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-01-20 15:22:20 +0800

    add vault factory setup

--- contracts/contracts/ethereum/ERC4626VaultFactory.sol
+++ contracts/contracts/ethereum/ERC4626VaultFactory.sol
@@ -1,6 +1,238 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract ERC4626VaultFactory{
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/IERC20.sol";
+
+error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees); // Used to make sure contract has enough balance.
+error NothingToWithdraw(); // Used when trying to withdraw Ether but there's nothing to withdraw.
+error FailedToWithdrawEth(address owner, address target, uint256 value); // Used when the withdrawal of Ether fails.
+error DestinationChainNotAllowlisted(uint64 destinationChainSelector); // Used when the destination chain has not been allowlisted by the contract owner.
+error SourceChainNotAllowlisted(uint64 sourceChainSelector); // Used when the source chain has not been allowlisted by the contract owner.
+error SenderNotAllowlisted(address sender); // Used when the sender has not been allowlisted by the contract owner.
+
+contract ERC4626VaultFactory is CCIPReceiver{
     
+    struct Vault{
+        uint256 fanTokenId;
+        uint256 creatorLensProfileId;
+        address moduleAddress;
+        address creator;
+        address rewardToken;
+        uint64 sourceChainSelector;
+    }
+
+    address public vaultImplementation;
+    address public owner;
+    mapping(address => Vault) private vaults;
+
+    IERC20 private s_linkToken;
+    bytes32 private s_lastReceivedMessageId; // Store the last received messageId.
+    bytes private s_lastReceivedData; // Store the last received data.
+
+    // Mapping to keep track of allowlisted destination chains.
+    mapping(uint64 => bool) public allowlistedDestinationChains;
+
+    // Mapping to keep track of allowlisted source chains.
+    mapping(uint64 => bool) public allowlistedSourceChains;
+
+    // Mapping to keep track of allowlisted senders.
+    mapping(address => bool) public allowlistedSenders;
+
+    constructor(address _vaultImplementation,address _router, address _link, uint64 _chainSelector,address _moduleAddress) CCIPReceiver(_router) {
+        vaultImplementation = _vaultImplementation;
+        s_linkToken = IERC20(_link);
+        allowlistedDestinationChains[_chainSelector] = true;
+        allowlistedSourceChains[_chainSelector] = true;
+        allowlistedSenders[_moduleAddress]=true;
+        owner=msg.sender;
+        emit OwnershipTransferred(address(0), owner);
+    }
+        // Event emitted when a message is sent to another chain.
+    event MessageSent(
+        bytes32 indexed messageId, // The unique ID of the CCIP message.
+        uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
+        address receiver, // The address of the receiver on the destination chain.
+        bytes data, // The data being sent.
+        address feeToken, // the token address used to pay CCIP fees.
+        uint256 fees // The fees paid for sending the CCIP message.
+    );
+
+    // Event emitted when a message is received from another chain.
+    event MessageReceived(
+        bytes32 indexed messageId, // The unique ID of the CCIP message.
+        uint64 indexed sourceChainSelector, // The chain selector of the source chain.
+        address sender, // The address of the sender from the source chain.
+        bytes data // The data that was received.
+    );
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event VaultDeployed(Vault indexed _vault);
+
+
+    // Modifers
+
+    /// @dev Modifier that checks if the sender is the owner of the contract.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Ownable: caller is not the owner");
+        _;
+    }
+    
+    /// @dev Modifier that checks if the chain with the given destinationChainSelector is allowlisted.
+    /// @param _destinationChainSelector The selector of the destination chain.
+    modifier onlyAllowlistedDestinationChain(uint64 _destinationChainSelector) {
+        if (!allowlistedDestinationChains[_destinationChainSelector])
+            revert DestinationChainNotAllowlisted(_destinationChainSelector);
+        _;
+    }
+
+    /// @dev Modifier that checks if the chain with the given sourceChainSelector is allowlisted and if the sender is allowlisted.
+    /// @param _sourceChainSelector The selector of the destination chain.
+    /// @param _sender The address of the sender.
+    modifier onlyAllowlisted(uint64 _sourceChainSelector, address _sender) {
+        if (!allowlistedSourceChains[_sourceChainSelector])
+            revert SourceChainNotAllowlisted(_sourceChainSelector);
+        if (!allowlistedSenders[_sender]) revert SenderNotAllowlisted(_sender);
+        _;
+    }
+
+    /// @notice Allow the contract owner to transfer his ownership.
+    /// @param newOwner The address of the new owner.
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        owner = newOwner;
+        emit OwnershipTransferred(owner, newOwner);
+    }
+
+    // @notice Allow the contract owner to add the cross chain module address that can deploy a vault. NOT FOR PRODUCTIION!
+    /// @param _moduleAddress The address of the module to be allowlisted.
+    function addModuleAddress(address _moduleAddress) public onlyOwner{
+        allowlistedSenders[_moduleAddress]=true;
+    }
+
+    // Contract Functions
+
+    // @notice Internal Function that deploys a vault on receiving a CCIP message from GHOFundMe module.
+    function _deployVault(
+        Vault memory _vault
+    ) internal returns (Vault memory) {
+        vaults[_vault.moduleAddress] = _vault;
+        emit VaultDeployed(_vault);
+        return _vault;
+    }
+    
+    // Chainlink CCIP functions
+
+    /// handle a received message
+    function _ccipReceive(
+        Client.Any2EVMMessage memory any2EvmMessage
+    )
+        internal
+        override
+        onlyAllowlisted(
+            any2EvmMessage.sourceChainSelector,
+            abi.decode(any2EvmMessage.sender, (address))
+        ) // Make sure source chain and sender are allowlisted
+    {
+        s_lastReceivedMessageId = any2EvmMessage.messageId; // fetch the messageId
+        s_lastReceivedData = any2EvmMessage.data; // abi-decoding of the sent data
+        _deployVault(abi.decode(any2EvmMessage.data, (Vault)));
+        emit MessageReceived(
+            any2EvmMessage.messageId,
+            any2EvmMessage.sourceChainSelector, // fetch the source chain identifier (aka selector)
+            abi.decode(any2EvmMessage.sender, (address)), // abi-decoding of the sender address,
+            any2EvmMessage.data
+        );
+    }
+
+    /// @notice Construct a CCIP message.
+    /// @dev This function will create an EVM2AnyMessage struct with all the necessary information for sending a data.
+    /// @param _receiver The address of the receiver.
+    /// @param _data The raw data to be sent.
+    /// @param _feeTokenAddress The address of the token used for fees. Set address(0) for native gas.
+    /// @return Client.EVM2AnyMessage Returns an EVM2AnyMessage struct which contains information for sending a CCIP message.
+    function _buildCCIPMessage(
+        address _receiver,
+        bytes calldata _data,
+        address _feeTokenAddress
+    ) internal pure returns (Client.EVM2AnyMessage memory) {
+        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
+        return
+            Client.EVM2AnyMessage({
+                receiver: abi.encode(_receiver), // ABI-encoded receiver address
+                data: abi.encode(_data), // ABI-encoded string
+                tokenAmounts: new Client.EVMTokenAmount[](0), // Empty array aas no tokens are transferred
+                extraArgs: Client._argsToBytes(
+                    // Additional arguments, setting gas limit
+                    Client.EVMExtraArgsV1({gasLimit: 200_000})
+                ),
+                // Set the feeToken to a feeTokenAddress, indicating specific asset will be used for fees
+                feeToken: _feeTokenAddress
+            });
+    }
+
+    /// @notice Fetches the details of the last received message.
+    /// @return messageId The ID of the last received message.
+    /// @return data The last received data.
+    function getLastReceivedMessageDetails()
+        external
+        view
+        returns (bytes32 messageId, bytes memory data)
+    {
+        return (s_lastReceivedMessageId, s_lastReceivedData);
+    }
+
+
+    // Funds Recovery functions
+
+    /// @notice Fallback function to allow the contract to receive Ether.
+    /// @dev This function has no function body, making it a default function for receiving Ether.
+    /// It is automatically called when Ether is sent to the contract without any data.
+    receive() external payable {}
+
+    /// @notice Allows the contract owner to withdraw the entire balance of Ether from the contract.
+    /// @dev This function reverts if there are no funds to withdraw or if the transfer fails.
+    /// It should only be callable by the owner of the contract.
+    /// @param _beneficiary The address to which the Ether should be sent.
+    function withdraw(address _beneficiary) public onlyOwner {
+        // Retrieve the balance of this contract
+        uint256 amount = address(this).balance;
+
+        // Revert if there is nothing to withdraw
+        if (amount == 0) revert NothingToWithdraw();
+
+        // Attempt to send the funds, capturing the success status and discarding any return data
+        (bool sent, ) = _beneficiary.call{value: amount}("");
+
+        // Revert if the send failed, with information about the attempted transfer
+        if (!sent) revert FailedToWithdrawEth(msg.sender, _beneficiary, amount);
+    }
+
+    /// @notice Allows the owner of the contract to withdraw all tokens of a specific ERC20 token.
+    /// @dev This function reverts with a 'NothingToWithdraw' error if there are no tokens to withdraw.
+    /// @param _beneficiary The address to which the tokens will be sent.
+    /// @param _token The contract address of the ERC20 token to be withdrawn.
+    function withdrawToken(
+        address _beneficiary,
+        address _token
+    ) public onlyOwner {
+        // Retrieve the balance of this contract
+        uint256 amount = IERC20(_token).balanceOf(address(this));
+
+        // Revert if there is nothing to withdraw
+        if (amount == 0) revert NothingToWithdraw();
+
+        IERC20(_token).transfer(_beneficiary, amount);
+    }
+
+
+
+
+
+
+    
+
+
 }
