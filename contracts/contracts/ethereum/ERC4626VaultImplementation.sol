f885a8c23df92d8d7402feb89954feb7d84f02f8
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-01-20 12:56:43 +0800

    setup ccip

--- contracts/contracts/ethereum/ERC4626VaultImplementation.sol
+++ contracts/contracts/ethereum/ERC4626VaultImplementation.sol
@@ -1,6 +1,104 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract ERC4626VaultImplementation{
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/IERC20.sol";
+
+error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees); // Used to make sure contract has enough balance.
+error NothingToWithdraw(); // Used when trying to withdraw Ether but there's nothing to withdraw.
+error FailedToWithdrawEth(address owner, address target, uint256 value); // Used when the withdrawal of Ether fails.
+error DestinationChainNotAllowlisted(uint64 destinationChainSelector); // Used when the destination chain has not been allowlisted by the contract owner.
+error SourceChainNotAllowlisted(uint64 sourceChainSelector); // Used when the source chain has not been allowlisted by the contract owner.
+error SenderNotAllowlisted(address sender); // Used when the sender has not been allowlisted by the contract owner.
+
+contract ERC4626VaultImplementation is CCIPReceiver{
+    bytes32 private s_lastReceivedMessageId; // Store the last received messageId.
+    address public owner;
+    string private s_lastReceivedText; // Store the last received text.
+
+    // Mapping to keep track of allowlisted destination chains.
+    mapping(uint64 => bool) public allowlistedDestinationChains;
+
+    // Mapping to keep track of allowlisted source chains.
+    mapping(uint64 => bool) public allowlistedSourceChains;
+
+    // Mapping to keep track of allowlisted senders.
+    mapping(address => bool) public allowlistedSenders;
+
+    IERC20 private s_linkToken;
+
+    // Event emitted when a message is sent to another chain.
+    event MessageSent(
+        bytes32 indexed messageId, // The unique ID of the CCIP message.
+        uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
+        address receiver, // The address of the receiver on the destination chain.
+        string text, // The text being sent.
+        address feeToken, // the token address used to pay CCIP fees.
+        uint256 fees // The fees paid for sending the CCIP message.
+    );
+
+    // Event emitted when a message is received from another chain.
+    event MessageReceived(
+        bytes32 indexed messageId, // The unique ID of the CCIP message.
+        uint64 indexed sourceChainSelector, // The chain selector of the source chain.
+        address sender, // The address of the sender from the source chain.
+        string text // The text that was received.
+    );
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    event Initialized(address indexed creator, uint256 indexed lensProfileId, address indexed moduleAddress, uint256 indexed chainSelector);
+
+    /// @notice Constructor initializes the contract with the router address.
+    /// @param _router The address of the router contract.
+    /// @param _link The address of the link contract.
+    constructor(address _router, address _link) CCIPReceiver(_router) {
+        s_linkToken = IERC20(_link);
+    }
+
+    
+    function initialize(address creator, uint256 lensProfileId,address moduleAddress,uint256 chainSelector) public {
+        require(owner == address(0), "already initialized");
+        owner = creator;
+        allowlistedDestinationChains[chainSelector] = true;
+        allowlistedSourceChains[chainSelector] = true;
+        emit OwnershipTransferred(address(0), creator);
+        emit Initialized(creator, lensProfileId, moduleAddress, chainSelector);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Ownable: caller is not the owner");
+        _;
+    }
+    
+    /// @dev Modifier that checks if the chain with the given destinationChainSelector is allowlisted.
+    /// @param _destinationChainSelector The selector of the destination chain.
+    modifier onlyAllowlistedDestinationChain(uint64 _destinationChainSelector) {
+        if (!allowlistedDestinationChains[_destinationChainSelector])
+            revert DestinationChainNotAllowlisted(_destinationChainSelector);
+        _;
+    }
+
+    /// @dev Modifier that checks if the chain with the given sourceChainSelector is allowlisted and if the sender is allowlisted.
+    /// @param _sourceChainSelector The selector of the destination chain.
+    /// @param _sender The address of the sender.
+    modifier onlyAllowlisted(uint64 _sourceChainSelector, address _sender) {
+        if (!allowlistedSourceChains[_sourceChainSelector])
+            revert SourceChainNotAllowlisted(_sourceChainSelector);
+        if (!allowlistedSenders[_sender]) revert SenderNotAllowlisted(_sender);
+        _;
+    }
+
+    /// @notice Allow the contract owner to transfer his ownership.
+    /// @param newOwner The address of the new owner.
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        owner = newOwner;
+        emit OwnershipTransferred(owner, newOwner);
+    }
+    
+
 
 }
