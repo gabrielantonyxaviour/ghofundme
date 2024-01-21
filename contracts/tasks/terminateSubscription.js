f1b3f370128d6f5804375b1251eb44fd19d78d55
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-01-21 16:35:15 +0800

    add terminate

--- contracts/tasks/terminateSubscription.js
+++ contracts/tasks/terminateSubscription.js
@@ -1,12 +1,14 @@
 const { DEFAULT_VERIFICATION_BLOCK_CONFIRMATIONS } = require("../networks")
 task("terminate-subscription", "Terminating a subscription to a creator using GHOFundMeModule").setAction(
   async (taskArgs) => {
+    const lensProfileId = 1105
+    const tokenAmount = 2
     const moduleAddress = networks.polygonMumbai.module
 
     const moduleFactory = await ethers.getContractFactory("GHOFundMeModule")
     const moduleContract = moduleFactory.attach(moduleAddress)
 
-    const tx = await moduleContract.setupModule()
+    const tx = await moduleContract.terminateSubscription(lensProfileId, tokenAmount)
 
     const receipt = await tx.wait(DEFAULT_VERIFICATION_BLOCK_CONFIRMATIONS)
 
