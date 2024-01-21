8686c440201edc8d27e16daf01675c079eb85ab4
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-01-21 13:15:40 +0530

    dashboard

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -7,31 +7,32 @@ import { useEffect, useState } from "react";
 import TransactionList from "@/components/Transaction";
 
 const page: NextPage = () => {
-    // const { address, isConnecting, isDisconnected } = useAccount()
-    // const { disconnect } = useDisconnect()
-    // const [lensid, setislensid] = useState("")
-    // useEffect(() => {
-    //   if(isDisconnected){redirect('/')}
-    // }, [isDisconnected])
+    const { address, isConnecting, isDisconnected } = useAccount()
+    const { disconnect } = useDisconnect()
+    const [lensid, setislensid] = useState("")
+    useEffect(() => {
+      if(isDisconnected){redirect('/')}
+    }, [isDisconnected])
     const [totalClaimableGho, setTotalClaimableGho] = useState(0)
     const [claimableWindow, setClaimableWindow] = useState(0)
     const [volumeOfFanTokenTraded, setVolumeOfFanTokenTraded] = useState(0)
     const [totalGhoStaked, setTotalGhoStaked] = useState(0)
-    const address = "0x1234567890123456789012345678901234567890"
   return (
     <div className="relative bg-gray-200 w-full h-screen overflow-hidden text-left text-5xl text-white font-monument-extended">
       {/* <div className="absolute top-[-241px] left-[1366px] w-[1366px] h-[1009px] [transform:_rotate(180deg)] [transform-origin:0_0]" /> */}
       <img
-        className="absolute top-[0px] left-[0px] w-screen h-screen object-cover"
+        className="absolute top-[1px] left-[0px] w-screen h-screen object-cover"
         alt=""
         src="/2303w018n0021741ap30-1@2x.png"
       />
       <div className="absolute top-[0px] left-[0px] bg-[#131212] w-screen    h-screen" />
+      
       <img
         className="absolute top-[28px] left-[51px] w-[422px] h-[123px] overflow-hidden"
         alt=""
         src="/ghfundme-1.svg"
       />
+      <div className="absolute left-32">
       <div className="absolute top-[42px] left-[1810px] w-14 h-14">
         <div className="absolute top-[0px] left-[0px]w-14 h-14" />
         <div className="absolute top-[21px] left-[14px] w-[29px] h-3.5">
@@ -57,32 +58,35 @@ const page: NextPage = () => {
           </div>
         </div>
         <div className="absolute top-[213px] left-[59px] text-xl inline-block w-[246px] h-6">
-        {/* {!isConnecting?(address?.substring(0, 10) + "......." + address?.substring(address?.length - 8)):
+        {!isConnecting?(address?.substring(0, 10) + "......." + address?.substring(address?.length - 8)):
         (<div className='flex space-x-2 absolute top-[0px] left-[109px] text-xl w-[246px] h-6'>
         <span className='sr-only'>Loading...</span>
          <div className='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
        <div className='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
        <div className='h-8 w-8 bg-white rounded-full animate-bounce'></div>
-   </div>)} */}
+   </div>)}
         </div>
         <img
           className="absolute h-[121px] w-[181px] top-[80.59%] right-[67.61%] bottom-[56.89%] left-[20.17%] max-w-full overflow-hidden max-h-full transform transition duration-500 
           hover:scale-105"
           alt=""
           src="/logoutbutton.svg"
-        //   onClick={() =>
-        //     {   disconnect()
-        //     }}
+          onClick={() =>
+            {   disconnect()
+            }}
         />
       </div>
+      </div>
+      <div className="absolute top-[231px] left-[816px] w-[894px] h-[603px] text-xl text-gray-100 font-sans">
       
-      <div className="absolute top-[231px] left-[616px] w-[1194px] h-[603px] text-xl text-gray-100 font-sans">
-      
-        <div className="absolute top-[0px] left-[0px] rounded-xl  box-border w-[1194px] h-[603px] border-[2px] border-dashed border-[#025231]" />
+        <div className="absolute top-[0px] left-[0px] rounded-xl  box-border w-[894px] h-[603px] border-[2px] border-dashed border-[#025231]" />
         <div className="absolute top-[49px] left-[56px] text-[28px] text-white inline-block w-[379px] h-[68px]">
           Dashboard
         </div>
-        <div className="absolute left-[750px] top-10">
+        <div className="absolute top-[49px] left-[510px] text-[28px] text-white inline-block w-[379px] h-[68px]">
+         Transactions
+        </div>
+        <div className="absolute left-[510px] h-[603px] top-24">
         <TransactionList />
         </div>
         <div className="absolute top-[100px] left-[50px] text-white text-lg bg-gray-800 p-6 rounded-xl shadow-md">
--- frontend/components/Transaction.tsx
+++ frontend/components/Transaction.tsx
@@ -8,7 +8,12 @@ interface Transaction {
 
 const mockTransactions: Transaction[] = [
   { id: 1, description: 'Transaction 1', amount: 50 },
+  { id: 2, description: 'Transaction 2', amount: -30 },
+  { id: 1, description: 'Transaction 1', amount: 50 },
   { id: 2, description: 'Transaction 2', amount: -30 },  { id: 2, description: 'Transaction 2', amount: -30 },
+  { id: 1, description: 'Transaction 1', amount: 50 },
+  { id: 2, description: 'Transaction 2', amount: -30 },  { id: 2, description: 'Transaction 2', amount: -30 },
+
 
 ];
 
@@ -20,11 +25,36 @@ const TransactionList: React.FC = () => {
   }, []);
 
   return (
-    <div className="custom-scrollbar max-h-96 overflow-y-auto scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-gray-100">
+    <div
+      className="h-[355px] w-[340px] overflow-y-auto bg-gray-800 rounded-xl"
+      style={{
+        scrollbarWidth: 'thin',
+        scrollbarColor: 'transparent transparent', // Firefox scrollbar color
+        overflow: '-moz-scrollbars-none', // Hide Firefox scrollbar
+      }}
+    >
+      <style>
+        {`
+          .custom-scrollbar::-webkit-scrollbar {
+            width: 8px;
+          }
+
+          .custom-scrollbar::-webkit-scrollbar-thumb {
+            background-color: transparent; /* Scrollbar thumb color */
+            border-radius: 10px;
+          }
+
+          .custom-scrollbar::-webkit-scrollbar-track {
+            background-color: #282c34; /* Scrollbar track color */
+            border-radius: 10px;
+          }
+        `}
+      </style>
+
       {transactions.map((transaction) => (
-        <div key={transaction.id} className="transaction-item border-b p-4">
-          <p className="font-semibold">Description: {transaction.description}</p>
-          <p>Amount: ${transaction.amount}</p>
+        <div key={transaction.id} className="transaction-item border rounded-xl p-4 text-xs">
+          <p className="font-normal text-sm">Description: {transaction.description}</p>
+          <p>Amount: {transaction.amount} GHO</p>
         </div>
       ))}
     </div>
